03b62769ae42026d0aa72ce75faf016eb50a85df
Taryn <taryn@taryn.codes>
2020-05-29 15:59:01 +1000

    feat(blog): new blog post and refactor
    This commit refactors the existing blog posts to have more concise file names, uses images within the site, and adds a new blog post

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -5,7 +5,7 @@ import { getSortedPostsData } from '../../lib/posts'
 import Link from 'next/link'
 import Date from '../../components/date'
 
-export default function Home({ allPostsData }) {
+export default function Blog({ allPostsData }) {
   return (
     <Layout>
       <Head>
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
               </Link>
               <br />
               <small className={utilStyles.lightText}>
-                <Date dateString={date} />
+              <Date dateString={date} />
               </small>
             </li>
           ))}
