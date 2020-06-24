52185f6223d4c6b709dec670519bba17c5f9a580
Taryn <taryn@taryn.codes>
2020-06-24 10:48:34 +1000

    chore(blog): update blog link

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -31,7 +31,7 @@ export default function Blog({ allPostsData }) {
         <h3>My writing on other sites</h3>
         <ul className="blog__externalList">
           <li><a className={utilStyles.linkStandard} href="https://medium.com/finder-tech/web-accessibility-standards-version-1-2ce65d5f4567">Web Accessibility Standards version 1</a></li>
-          <li><a className={utilStyles.linkStandard} href="https://www.projectf.com.au/blog/2020/1/30/alternative-paths-to-software-engineering-for-women">Alternative paths to software engineering for women</a></li>
+          <li><a className={utilStyles.linkStandard} href="https://www.projectf.com.au/blog-collection/2020/1/30/alternative-paths-to-software-engineering-for-women">Alternative paths to software engineering for women</a></li>
           <li><a className={utilStyles.linkStandard} href="https://medium.com/finder-tech/imposter-syndrome-all-the-way-up-9d90753ad38c">Imposter syndrome all the way up</a></li>
           <li><a className={utilStyles.linkStandard} href="https://medium.com/finder-tech/analysing-our-engineering-teams-key-values-e9f79dd4391e">Analysing our engineering teams key values</a></li>
         </ul>
