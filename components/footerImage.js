ac57741de31e6043464ec33c96ca2f8b9f3feb16
tarynewens <taryn@taryn.codes>
2021-01-19 12:59:02 +1100

    update css images to be stored locally

--- components/footerImage.js
+++ components/footerImage.js
@@ -1,4 +1,4 @@
-import styles from './footerImage.module.css'
+import styles from "./footerImage.module.css";
 
 export default function FooterImage() {
   let imageOfThePage = pickImage();
@@ -9,44 +9,51 @@ export default function FooterImage() {
   function pickImage() {
     let images = [
       {
-        link: 'https://codepen.io/tarynewens/pen/KbeNYX',
-        src: 'https://tarynewens.com/portfolio/wp-content/uploads/2019/12/gelato.png',
-        alt: "CSS image of a gelato cone with happy faces"
+        link: "https://codepen.io/tarynewens/pen/KbeNYX",
+        src: "/cssImages/gelato.png",
+        alt: "CSS image of a gelato cone with happy faces",
       },
       {
-        link: 'https://codepen.io/tarynewens/pen/RzgOgG',
-        src: 'https://tarynewens.com/portfolio/wp-content/uploads/2019/12/macarons.png',
-        alt: "CSS image of 3 macarons with happy faces"
+        link: "https://codepen.io/tarynewens/pen/RzgOgG",
+        src: "/cssImages/macarons.png",
+        alt: "CSS image of 3 macarons with happy faces",
       },
       {
-        link: 'https://codepen.io/tarynewens/pen/OKbWKy',
-        src: 'https://tarynewens.com/portfolio/wp-content/uploads/2019/12/plant1.png',
-        alt: "CSS image of a plant in a pink pot"
+        link: "https://codepen.io/tarynewens/pen/OKbWKy",
+        src: "/cssImages/plant1.png",
+        alt: "CSS image of a plant in a pink pot",
       },
       {
-        link: 'https://codepen.io/tarynewens/pen/rEqONM',
-        src: 'https://tarynewens.com/portfolio/wp-content/uploads/2019/12/plant2.png',
-        alt: "CSS image of a plant in a pink pot"
+        link: "https://codepen.io/tarynewens/pen/rEqONM",
+        src: "/cssImages/plant2.png",
+        alt: "CSS image of a plant in a pink pot",
       },
       {
-        link: 'https://codepen.io/tarynewens/pen/WmwgJR',
-        src: 'https://tarynewens.com/portfolio/wp-content/uploads/2019/12/pig.png',
-        alt: "CSS image of a geometric pig"
+        link: "https://codepen.io/tarynewens/pen/WmwgJR",
+        src: "/cssImages/pig.png",
+        alt: "CSS image of a geometric pig",
       },
       {
-        link: 'https://codepen.io/tarynewens/pen/WPgwZO',
-        src: 'https://tarynewens.com/portfolio/wp-content/uploads/2019/12/heart.png',
-        alt: "CSS image of a geometric heart"
-      }
-    ]
+        link: "https://codepen.io/tarynewens/pen/WPgwZO",
+        src: "/cssImages/heart.png",
+        alt: "CSS image of a geometric heart",
+      },
+    ];
 
     let n = Math.floor(Math.random() * images.length);
     return images[n];
   }
-  
+
   return (
-    <div className={styles.footerImageWrapper} >
-      <a href={ linkUrl } rel="noopener noreferrer" target="_blank" className={styles.footerImageLink}><img src={ src } alt={ alt } className={styles.footerImage}/></a>
+    <div className={styles.footerImageWrapper}>
+      <a
+        href={linkUrl}
+        rel="noopener noreferrer"
+        target="_blank"
+        className={styles.footerImageLink}
+      >
+        <img src={src} alt={alt} className={styles.footerImage} />
+      </a>
     </div>
-  )
+  );
 }
